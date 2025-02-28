091c4a5ef34c2f9c981e1311c391675eb93e1bf9
Nat Lownes <nathaniel.lownes@phila.gov>
2025-02-28 14:49:57 -0500

    add item id, retab

--- fs/onedrivefs/opener.py
+++ fs/onedrivefs/opener.py
@@ -9,32 +9,32 @@ from fs.opener import Opener
 from .onedrivefs import OneDriveFS
 
 def _SaveToken(_):
-	pass
+  pass
 
 class OneDriveFSOpener(Opener):
-	protocols: ClassVar[list[str]] = ['onedrive']
+  protocols: ClassVar[list[str]] = ['onedrive']
 
-	def open_fs(self, fs_url, parse_result, writeable, create, cwd): # noqa: ARG002
-		directory = parse_result.resource
+  def open_fs(self, fs_url, parse_result, writeable, create, cwd): # noqa: ARG002
+    directory = parse_result.resource
 
-		# this is missing various fields that hopefully aren't necessary
-		token = {
-			'token_type': 'Bearer',
-			'access_token': parse_result.params.get('access_token'),
-			'refresh_token': parse_result.params.get('refresh_token')
-		}
+    # this is missing various fields that hopefully aren't necessary
+    token = {
+      'token_type': 'Bearer',
+      'access_token': parse_result.params.get('access_token'),
+      'refresh_token': parse_result.params.get('refresh_token')
+    }
 
-		fs = OneDriveFS(
-			clientId=parse_result.params['client_id'],
-			clientSecret=parse_result.params.get('client_secret'),
-			token=token,
-			SaveToken=_SaveToken,
-			driveId=parse_result.params.get('drive_id'),
-			userId=parse_result.params.get('user_id'),
-			groupId=parse_result.params.get('group_id'),
-			siteId=parse_result.params.get('site_id'),
-		)
+    fs = OneDriveFS(
+      clientId=parse_result.params['client_id'],
+      clientSecret=parse_result.params.get('client_secret'),
+      token=token,
+      SaveToken=_SaveToken,
+      driveId=parse_result.params.get('drive_id'),
+      userId=parse_result.params.get('user_id'),
+      groupId=parse_result.params.get('group_id'),
+      siteId=parse_result.params.get('site_id'),
+    )
 
-		if directory:
-			return fs.opendir(directory)
-		return fs
+    if directory:
+      return fs.opendir(directory)
+    return fs
--- tests/github.py
+++ tests/github.py
@@ -7,25 +7,25 @@ from requests import get, put
 from requests.auth import HTTPBasicAuth
 
 def _EncryptForGithubSecret(publicKey, secretValue):
-	publicKey = public.PublicKey(publicKey.encode('utf-8'), encoding.Base64Encoder())
-	sealedBox = public.SealedBox(publicKey)
-	encrypted = sealedBox.encrypt(secretValue.encode('utf-8'))
-	return b64encode(encrypted).decode('utf-8')
+  publicKey = public.PublicKey(publicKey.encode('utf-8'), encoding.Base64Encoder())
+  sealedBox = public.SealedBox(publicKey)
+  encrypted = sealedBox.encrypt(secretValue.encode('utf-8'))
+  return b64encode(encrypted).decode('utf-8')
 
 def UploadSecret(token):
-	# needs a PAT with permissions to public repositories
-	auth = HTTPBasicAuth(environ['XGITHUB_USERNAME'], environ['XGITHUB_API_PERSONAL_TOKEN'])
-	headers = {'Accept': 'application/vnd.github.v3+json'}
+  # needs a PAT with permissions to public repositories
+  auth = HTTPBasicAuth(environ['XGITHUB_USERNAME'], environ['XGITHUB_API_PERSONAL_TOKEN'])
+  headers = {'Accept': 'application/vnd.github.v3+json'}
 
-	owner = environ['XGITHUB_REPO_OWNER']
-	baseUrl = f'https://api.github.com/repos/{owner}/fs.onedrivefs/actions/secrets'
-	publicKey = get(f'https://api.github.com/repos/{owner}/fs.onedrivefs/actions/secrets/public-key', headers=headers, auth=auth, timeout=30).json()
+  owner = environ['XGITHUB_REPO_OWNER']
+  baseUrl = f'https://api.github.com/repos/{owner}/fs.onedrivefs/actions/secrets'
+  publicKey = get(f'https://api.github.com/repos/{owner}/fs.onedrivefs/actions/secrets/public-key', headers=headers, auth=auth, timeout=30).json()
 
-	data = {
-		'encrypted_value': _EncryptForGithubSecret(publicKey['key'], dumps(token)),
-		'key_id': publicKey['key_id']
-		}
+  data = {
+    'encrypted_value': _EncryptForGithubSecret(publicKey['key'], dumps(token)),
+    'key_id': publicKey['key_id']
+    }
 
-	response = put(f'{baseUrl}/GRAPH_API_TOKEN_READONLY', headers=headers, data=dumps(data), auth=auth, timeout=30)
-	response.raise_for_status()
-	print('Uploaded key to Github')
+  response = put(f'{baseUrl}/GRAPH_API_TOKEN_READONLY', headers=headers, data=dumps(data), auth=auth, timeout=30)
+  response.raise_for_status()
+  print('Uploaded key to Github')
